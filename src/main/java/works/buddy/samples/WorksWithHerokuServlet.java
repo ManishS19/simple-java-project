55ee533b7cf77cbafb2fc69daf999be80e187fe5
Manish19 <103111275+ManishS19@users.noreply.github.com>
2022-06-15 19:40:34 +0530

    Update WorksWithHerokuServlet.java

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -13,7 +13,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("MONISH GOING TO HYDERABAD");
+        writer.print("MONISH IS GOING TO HYDERABAD");
         writer.close();
     }
 }
