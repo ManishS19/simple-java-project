7800724db5c502c544ca89dc2212588a3a3fb2bd
Manish19 <103111275+ManishS19@users.noreply.github.com>
2022-06-15 19:41:03 +0530

    Update WorksWithHerokuServletTest.java

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("MONISH GOING TO HYDERABAD", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("MONISH IS GOING TO HYDERABAD", new String( out.toByteArray(), "UTF-8"));
     }
 }
